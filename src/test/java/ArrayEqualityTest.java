1b959ce2d7d91b20059405e5dd2383d2aee23476
Umut Uygur <umut.uygur30@gmail.com>
2024-09-10 21:06:48 +0300

    Week3_HomeAssignment task

--- src/test/java/ArrayEqualityTest.java
+++ src/test/java/ArrayEqualityTest.java
@@ -1,45 +1,46 @@
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class ArrayEqualityTest {
 
+    static int[] sharedResource;
+
+    @BeforeAll
+    public static void setUpBeforeClass() {
+        System.out.println("Setting up resources which is shared by all tests");
+        sharedResource = new int[] {1, 2, 3};
+    }
+
+    @AfterAll
+    public static void tearDownAfterClass() {
+        System.out.println("Cleaning up resources which is shared by all tests");
+        sharedResource = null;
+    }
+
+    @BeforeEach
+    public void setUp() {
+        System.out.println("Setting up before every test");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("Cleaning up after every test");
+    }
+
     @Test
-    public void testSingleDimensionalArrayEquality() {
+    public void testSingleSizedArrayMatch() {
+        System.out.println("Running testSizedDimensionalArrayEquality");
         int[] a1 = { 2, 3, 5, 7 };
         int[] a2 = { 2, 3, 5, 7 };
-        assertArrayEquals(a1, a2, "Arrays should be equal");
+        assertArrayEquals(a1, a2, "Arrays has to be equal");
     }
 
     @Test
-    public void testSingleDimensionalArrayInequality() {
-        int[] a1 = { 2, 3, 5, 7 };
-        int[] a2 = { 2, 3, 5, 9 };
-        assertArrayEquals(a1, a2, "Arrays should not be equal");
+    public void testMultiSizedArrayEquality() {
+        System.out.println("Running testMultiSizedArrayEquality");
+        int[][] a11 = { { 2, 3 }, { 5, 7 }, { 11, 13 } };
+        int[][] a12 = { { 2, 3 }, { 5, 7 }, { 11, 13 } };
+        assertArrayEquals(a11, a12, "2D arrays has to be equal");
     }
 
-    @Test
-    public void testMultiDimensionalArrayEquality() {
-        int[][] a11 = { { 3, 3 }, { 7, 7 }, { 13, 13 } };
-        int[][] a12 = { { 3, 3 }, { 7, 7 }, { 13, 13 } };
-        assertArrayEquals(a11, a12, "2D arrays should be equal");
-    }
-
-    @Test
-    public void testMultiDimensionalArrayInequality() {
-        int[][] a11 = { { 3, 3 }, { 7, 7 }, { 13, 13 } };
-        int[][] a12 = { { 3, 3 }, { 9, 9 }, { 13, 13 } };
-        assertArrayEquals(a11, a12, "2D arrays should not be equal");
-    }
-
-    @Test
-    public void testDifferentTypeArrayEquality() {
-
-        String[] s1 = { "apple", "banana", "cherry" };
-        String[] s2 = { "apple", "banana", "cherry" };
-        assertArrayEquals(s1, s2, "String arrays should be equal");
-
-        double[] d1 = { 1.1, 2.2, 3.3 };
-        double[] d2 = { 1.1, 2.2, 3.3 };
-        assertArrayEquals(d1, d2, "Double arrays should be equal");
-    }
 }
